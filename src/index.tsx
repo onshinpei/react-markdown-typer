a0b6584ceee3a99c8add87cdf73cfb126ff5e1a7
onshinpei <244221001@qq.com>
2025-10-22 21:49:41 +0800

    Enhance type exports in index.tsx by adding IntervalType to improve type handling for Markdown components.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import MarkdownTyperCMD from './MarkdownTyperCMD';
 import MarkdownTyper from './MarkdownTyper';
-import type { MarkdownTyperCMDRef, MarkdownTyperRef, ITypedChar, MarkdownTyperProps, MarkdownTyperCMDProps } from './defined';
+import type { MarkdownTyperCMDRef, MarkdownTyperRef, ITypedChar, MarkdownTyperProps, MarkdownTyperCMDProps, IntervalType } from './defined';
 
 export default MarkdownTyper;
-export type { MarkdownTyperCMDRef, MarkdownTyperRef, ITypedChar, MarkdownTyperProps, MarkdownTyperCMDProps };
+export type { MarkdownTyperCMDRef, MarkdownTyperRef, ITypedChar, MarkdownTyperProps, MarkdownTyperCMDProps, IntervalType };
 export { MarkdownTyper, MarkdownTyperCMD };
